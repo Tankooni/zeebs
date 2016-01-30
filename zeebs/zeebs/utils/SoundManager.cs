4eee88bb26294dcf4ac183d4089fbcdd0b3b6de4
Zack Wheeler <913966+Tankooni@users.noreply.github.com>
2016-01-29 20:38:49 -0500

    Stuff

---
+++ zeebs/zeebs/utils/SoundManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Indigo;
+using Indigo.Audio;
+using System.IO;
+using System.Threading;
+using zeebs;
+
+public static class SoundManager
+{
+	public static Sound CurrentSong;
+	public static string CloboboboSongName = "";
+	public static float MusicVolume { get; set; }
+	private static Dictionary<string, Sound> musics = new Dictionary<string, Sound>();
+	private static Dictionary<string, Sound> sounds = new Dictionary<string, Sound>();
+	public static void Init(float musicVolume)
+	{
+		MusicVolume = FP.Clamp(musicVolume, 0, 1);
+		foreach (string file in Utility.RetrieveFilePathForFilesInDirectory(@"./content/music", @"*.ogg|*.wav"))
+		{
+			var sound = new Sound(Library.GetSoundStream(file));
+			sound.OnComplete += LoopMusic;
+			musics.Add(Path.GetFileNameWithoutExtension(file), sound);
+		}
+
+		foreach (string file in Utility.RetrieveFilePathForFilesInDirectory(@"./content/sounds", @"*.ogg|*.wav"))
+			sounds.Add(Path.GetFileNameWithoutExtension(file), new Sound(Library.GetSoundBuffer(file)));
+	}
+
+	private static void LoopMusic()
+	{
+		if (CurrentSong != null)
+			CurrentSong.Stop();
+		Sound newSong = musics[CloboboboSongName];
+		newSong.Volume = MusicVolume;
+		CurrentSong = newSong;
+		CurrentSong.Play();
+	}
+
+	public static void PlayMusic(string music)
+	{
+		Utility.MainConfig.CurrentMusic = CloboboboSongName = music;
+		if (CurrentSong != null)
+			CurrentSong.Stop();
+		Sound newSong = musics[music];
+		newSong.Volume = MusicVolume;
+		CurrentSong = newSong;
+		CurrentSong.Play();
+	}
+
+	public static void PlaySound(string soundName)
+	{
+		sounds[soundName].Play();
+
+	}
+
+	/// <summary>
+	/// Plays a sound with some volume varience
+	/// </summary>
+	/// <param name="soundName">Name of sound to play</param>
+	/// <param name="minimumVolume">0 to 1</param>
+	/// <param name="maxVolume">0 to 1</param>
+	public static void PlaySoundVariations(string soundName, float minimumVolume, float maxVolume)
+	{
+		sounds[soundName].Volume = (FP.Random.Float((int)((maxVolume - minimumVolume) * 100.0f)) / 100.0f) + minimumVolume;
+		//sounds[soundName].Volume = (FP.Rand(100 - (int)minimumVolume*100) + (int)minimumVolume)/100.0f;
+		sounds[soundName].Play();
+	}
+}
---
+++ zeebs/zeebs/worlds/StartScreenWorld.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Indigo;
+using Indigo.Inputs;
+using Indigo.Graphics;
+
+namespace zeebs
+{
+	class StartScreenWorld : World
+	{
+		private Text start;
+		private Text instructions;
+
+		public StartScreenWorld()
+		{
+			start = new Text("Start [Enter]");
+			start.X = (FP.Width / 2) - (start.Width / 2);
+			start.Y = (FP.Height / 3) + 25;
+
+			instructions = new Text("Instructions [Space]");
+			instructions.X = (FP.Width / 2) - (instructions.Width / 2);
+			instructions.Y = (FP.Height / 3) + 50;
+			AddGraphic(start);
+		}
+
+		public override void Update()
+		{
+			base.Update();
+			//if (Keyboard.Return.Pressed)
+			//	FP.World = new DynamicSceneWorld(Utility.MainConfig.StartingScene, Utility.MainConfig.SpawnEntrance);
+
+			//            if (Keyboard.Space.Pressed)
+			//                FP.World = new InstructionsScreenWorld();
+		}
+	}
+}
