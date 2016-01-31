b390d7bfafebf6bdec9979a26760318bb1205d1b
lindenk <lindenk@localhost>
2016-01-31 14:25:29 -0500

    Added flip (sorta kinda isn't perfect)

---
+++ zeebs/zeebs/entities/ComEntities/Commands/ComEntityFlip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zeebs.entities.ComEntities.Commands
+{
+    public class ComEntityFlip : ComEntityCommand
+    {
+        bool isDone = false;
+        public ComEntityFlip(ComEntity comEntity)
+            : base(comEntity)
+        { }
+
+        public override bool IsDone()
+        {
+            return isDone;
+        }
+
+        public override void Update()
+        {
+            comEntity.IsFlipped = !comEntity.IsFlipped;
+            isDone = true;
+        }
+    }
+}
