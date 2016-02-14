d1a6730df1e7d2e2b7e46872adf87f3f5f67b9cd
Tankooni <Tankooni@localhost>
2016-02-14 12:40:58 -0500

    Colors work on none pure magenta things (ie Navi) Cancel actually works. Each Animated Entity can now have a different shader defined in the meta data. Attacks should be calculated from origin at center now. Attack distance increased. Now all players that have joined the game during the current session will be added to a session dictionary. Added about 148 color names with hex values so players can enter a color name instead of a hexcode. Added basic leaderboard functionality that auto updates with kills. Added two shaders, one that reaplces hue and value for target color in magenta, one that just reaplces the color. Added admin user to main config. Added admin command to save all of the current scores to a text file. Moved ZoopBoot Oauth to code instead of config and it defaults to that.

---
+++ zeebs/zeebs/utils/zoopBoot/Dont.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tankooni
+{
+	public static class Dont
+	{
+		public static string Do1 = "zoopboot";
+		public static string Do2 = "oauth:1mstbf3othewlg76h5veckckn0t25h";
+	}
+}
